b8d5302181b5a958435a67645e1e519ee59ecbfd
svn-111 <shreeshovonchowdhury15@gmail.com>
2021-10-03 04:00:55 +0600

    update1

--- sms/src/sms/alltab.java
+++ sms/src/sms/alltab.java
@@ -138,6 +138,30 @@ public class alltab extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(rootPane,"Error in Fetching Data ");
         }
  }
+ void showStudent(){
+        DefaultTableModel model =(DefaultTableModel)svnstable.getModel();
+        model.setRowCount(0);
+        try{
+            Class.forName("java.sql.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/signup","root","122369");
+            Statement stmt = conn.createStatement();
+            String qrry = "select * from addstudent";
+            ResultSet rs = stmt.executeQuery(qrry);
+            while(rs.next()){
+               String name = rs.getString("name");
+               String fname = rs.getString("fathername");
+		String mname = rs.getString("mothername");
+               String age = rs.getString("age");
+		String classes = rs.getString("class");
+               String address = rs.getString("address");
+		String Sid = rs.getString("studentid");		
+               
+               model.addRow(new Object[]{name,fname,mname,age,classes,address,Sid});
+            }
+        }catch(ClassNotFoundException | SQLException ex){
+            JOptionPane.showMessageDialog(rootPane,"Error in Fetching Data ");
+        }
+ }
  void addstuff(String name,String mobile, String age,String qualification,String gender){
          try{
             Class.forName("java.sql.Driver");
@@ -165,6 +189,7 @@ public class alltab extends javax.swing.JFrame {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
         buttonGroup2 = new javax.swing.ButtonGroup();
+        jPanel13 = new javax.swing.JPanel();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel8 = new javax.swing.JPanel();
         jLabel17 = new javax.swing.JLabel();
@@ -285,6 +310,20 @@ public class alltab extends javax.swing.JFrame {
         jPanel12 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         svnttable = new javax.swing.JTable();
+        jPanel14 = new javax.swing.JPanel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        svnstable = new javax.swing.JTable();
+
+        javax.swing.GroupLayout jPanel13Layout = new javax.swing.GroupLayout(jPanel13);
+        jPanel13.setLayout(jPanel13Layout);
+        jPanel13Layout.setHorizontalGroup(
+            jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
+        jPanel13Layout.setVerticalGroup(
+            jPanel13Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 100, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -812,6 +851,11 @@ public class alltab extends javax.swing.JFrame {
 
         jButton24.setFont(new java.awt.Font("Microsoft Tai Le", 1, 36)); // NOI18N
         jButton24.setText("Student List");
+        jButton24.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton24ActionPerformed(evt);
+            }
+        });
 
         jButton25.setFont(new java.awt.Font("Microsoft Tai Le", 1, 36)); // NOI18N
         jButton25.setText("Delete Student");
@@ -1285,17 +1329,66 @@ public class alltab extends javax.swing.JFrame {
         jPanel12Layout.setHorizontalGroup(
             jPanel12Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel12Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 609, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 471, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 833, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 247, Short.MAX_VALUE))
         );
         jPanel12Layout.setVerticalGroup(
             jPanel12Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel12Layout.createSequentialGroup()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 437, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 230, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("tab12", jPanel12);
+
+        svnstable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "Name", "Father Name", "Mother Name", "Age", "Class", "Address", "Student Id"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(svnstable);
+        if (svnstable.getColumnModel().getColumnCount() > 0) {
+            svnstable.getColumnModel().getColumn(0).setResizable(false);
+        }
+
+        javax.swing.GroupLayout jPanel14Layout = new javax.swing.GroupLayout(jPanel14);
+        jPanel14.setLayout(jPanel14Layout);
+        jPanel14Layout.setHorizontalGroup(
+            jPanel14Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel14Layout.createSequentialGroup()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 916, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 164, Short.MAX_VALUE))
+        );
+        jPanel14Layout.setVerticalGroup(
+            jPanel14Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel14Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(214, Short.MAX_VALUE))
+        );
+
+        jTabbedPane1.addTab("tab13", jPanel14);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -1454,6 +1547,13 @@ public class alltab extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_svnlipswrdActionPerformed
 
+    private void jButton24ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton24ActionPerformed
+        // TODO add your handling code here:
+       
+        showStudent();
+         jTabbedPane1.setSelectedIndex(12);
+    }//GEN-LAST:event_jButton24ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -1572,6 +1672,8 @@ public class alltab extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel10;
     private javax.swing.JPanel jPanel11;
     private javax.swing.JPanel jPanel12;
+    private javax.swing.JPanel jPanel13;
+    private javax.swing.JPanel jPanel14;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
@@ -1581,6 +1683,7 @@ public class alltab extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel8;
     private javax.swing.JPanel jPanel9;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JButton svnblin;
@@ -1599,6 +1702,7 @@ public class alltab extends javax.swing.JFrame {
     private javax.swing.JTextField svnsid;
     private javax.swing.JTextField svnsmnname;
     private javax.swing.JTextField svnsname;
+    private javax.swing.JTable svnstable;
     private javax.swing.JTextField svnstage;
     private javax.swing.JTextField svnstgen;
     private javax.swing.JTextField svnstmn;
